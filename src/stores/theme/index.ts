5e695eaba6d8f8ebf4fc1f09907dc9416d730570
Bob McAleavey <bobmcaleavey@gmail.com>
2024-01-30 13:33:37 -0500

    fix(theme): change input text color

--- src/stores/theme/index.ts
+++ src/stores/theme/index.ts
@@ -7,7 +7,7 @@ theme.set({
 	buttonIcon: 'px-3',
 	card: 'bg-neutral-100 dark:bg-neutral-900',
 	input:
-		'dark:text-neutral-900 focus:ring-offset-violet-500 focus:ring-violet-500/30 hover:ring-offset-violet-500 ring-offset-neutral-300 text-current',
+		'dark:text-neutral-900 focus:ring-offset-violet-500 focus:ring-violet-500/30 hover:ring-offset-violet-500 ring-offset-neutral-300 text-neutral-900 dark:text-neutral-100',
 	nav: 'flex-row',
 	overlay: 'fixed',
 	table: 'dark:bg-neutral-900',
--- src/stores/weeks/index.ts
+++ src/stores/weeks/index.ts
@@ -5,23 +5,23 @@ import { settings } from '$stores';
 export const weeks = derived(
 	settings,
 	($settings) =>
-		[...Array($settings.years * 52)].reduce(
+		[...Array(+$settings.years * 52)].reduce(
 			({ data, weeks }, _, weekIndex) => {
 				data.week = weekIndex + 1;
 				data.year = Math.floor(weekIndex / 52) + 1;
 				data.date = DateTime.fromISO($settings.startDate).plus({ days: weekIndex * 7 });
 				data.age = data.date.diff(DateTime.fromISO('1985-04-11'), 'years').toObject().years;
 				data.reinvestment =
-					$settings.weeklyReinvestment *
-					Math.pow(1 + $settings.annualIncreasePercent, data.year - 1);
+					parseFloat($settings.weeklyReinvestment) *
+					Math.pow(1 + parseFloat($settings.annualIncreasePercent), data.year - 1);
 				data.totalInvested =
-					(data?.totalInvested || $settings.initialInvestment) + data.reinvestment;
+					(data?.totalInvested || parseFloat($settings.initialInvestment)) + data.reinvestment;
 				data.portfolio = (data?.portfolio || data.totalInvested) + data.reinvestment;
-				data.shares = data.portfolio / $settings.strikePrice;
+				data.shares = data.portfolio / parseFloat($settings.strikePrice);
 				data.lastDayOfMonth = data.date.endOf('month');
 				data.isLastWeekOfMonth = data.lastDayOfMonth.diff(data.date, 'days').days < 7;
 				data.dividendPayout = data.isLastWeekOfMonth
-					? data.shares * $settings.dividendRate
+					? data.shares * parseFloat($settings.dividendRate)
 					: data?.dividendPayout || 0;
 				data.portfolio += data.isLastWeekOfMonth ? data.dividendPayout : 0;
 				data.profit = data.portfolio - data.totalInvested;
