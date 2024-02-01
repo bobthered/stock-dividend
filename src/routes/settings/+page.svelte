faef9da7770482fc69116e638c3d2fe9f708582f
Bob McAleavey <bobmcaleavey@gmail.com>
2024-02-01 07:30:53 -0500

    fix(settings): change inputs inputmode to decimal

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,25 +9,25 @@
 	<H1>Settings</H1>
 	<div class="-mx-6 flex flex-grow flex-col space-y-3 overflow-auto px-6 pb-6">
 		<Fieldset legend="Strike Price">
-			<Input bind:value={$settings.strikePrice} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.strikePrice} inputmode="decimal" type="number" />
 		</Fieldset>
 		<Fieldset legend="Initial Investment">
-			<Input bind:value={$settings.initialInvestment} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.initialInvestment} inputmode="decimal" type="number" />
 		</Fieldset>
 		<Fieldset legend="Dividend Rate">
-			<Input bind:value={$settings.dividendRate} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.dividendRate} inputmode="decimal" type="number" />
 		</Fieldset>
 		<Fieldset legend="Weekly Reinvestment">
-			<Input bind:value={$settings.weeklyReinvestment} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.weeklyReinvestment} inputmode="decimal" type="number" />
 		</Fieldset>
 		<Fieldset legend="Annual Increase %">
-			<Input bind:value={$settings.annualIncreasePercent} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.annualIncreasePercent} inputmode="decimal" type="number" />
 		</Fieldset>
 		<Fieldset legend="Start Date">
 			<Input bind:value={$settings.startDate} type="date" />
 		</Fieldset>
 		<Fieldset legend="Years">
-			<Input bind:value={$settings.years} pattern="(\d|\.)*" type="number" />
+			<Input bind:value={$settings.years} inputmode="decimal" type="number" />
 		</Fieldset>
 	</div>
 </div>
